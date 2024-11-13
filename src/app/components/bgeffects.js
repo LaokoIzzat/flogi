2797a7e08ff3af66682065306432c80c67c2c0f3
habib-wahab <gh@habibwahab.co.uk>
2024-11-13 17:09:13 +0000

    increase particle opacity

--- src/app/components/bgeffects.js
+++ src/app/components/bgeffects.js
@@ -113,7 +113,7 @@ const BGEffects = () => {
   return (
     <canvas
       ref={canvasRef}
-      className="fixed top-0 left-0 w-full h-full pointer-events-none opacity-40"
+      className="fixed top-0 left-0 w-full h-full pointer-events-none opacity-80"
       style={{ zIndex: 0 }}
     />
   );
