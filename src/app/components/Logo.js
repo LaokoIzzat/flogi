438b3a17d743290de364c78aaa8b6669f3107581
habib-wahab <gh@habibwahab.co.uk>
2024-12-08 01:59:43 +0000

    temp changes for laoko

--- src/app/components/Logo.js
+++ src/app/components/Logo.js
@@ -1,15 +1,18 @@
-'use client';
-
 import { useRef } from 'react';
 
 export default function Logo() {
   return (
     <div className="w-48 h-48 relative group">
-      <img
-        src="/images/flogi-logo.gif"
-        alt="Logo"
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
         className="w-full h-full object-contain drop-shadow-2xl transition-transform duration-700 ease-out [@media(hover:hover)]:group-hover:scale-110"
-      />
+      >
+        <source src="/images/flogi-logo.webm" type="video/webm" />
+        <source src="/images/input.mov" type="video/mov" />
+      </video>
     </div>
   );
 }
